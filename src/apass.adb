123049ad97b25b613c6cd17d72dd8594c06dff9a
Simon Rowe <simon@wiremoons.com>
2021-02-05 13:59:26 +0000

    Add debug only message to ensure user is aware.

--- src/apass.adb
+++ src/apass.adb
@@ -9,14 +9,20 @@ with Ada.Text_IO; use Ada.Text_IO;
 -- use local packages
 with Password_Manager;
 
-procedure apass is
+procedure Apass is
 --------------------------
 -- MAIN
 --------------------------
 begin
 
-   Put_Line ("Program starts...");
+   --  print info on how to compile a 'release' version
+   pragma Debug
+      (Put_Line
+          (Standard_Error,
+           "DEBUG: build a 'release' version with: gprclean && gprbuild -XBUILD=release"));
+
+   Put_Line ("Program 'apass' running...");
    Put_Line ("Total words available: " & Password_Manager.Total_Words);
    Put_Line (Password_Manager.Basic_Password (3));
 
-end apass;
+end Apass;
